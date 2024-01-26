cb22c53323200a73caf6c86e736c6eb9815a354e
marie-anne-decarie <157767089+marie-anne-decarie@users.noreply.github.com>
2024-01-26 08:44:48 -0500

    class 2

---
+++ SampleWeek2/Assets/Scripts/BedController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedController : MonoBehaviour
+
+
+
+{
+    public float xLoc = 0;
+    public float xSpeed = 0.1f;
+    public GameObject bed;
+    public SpriteRenderer sr;
+    public int score;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z) && xLoc > -11f) 
+        {
+        xLoc -= xSpeed;
+        }
+    if (Input.GetKey(KeyCode.X) && xLoc < 11f)
+
+    {
+        xLoc += xSpeed;
+    }
+
+        this.transform.position = new Vector3(
+            xLoc,
+            transform.position.y,
+            transform.position.z);
+
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "Sleepy")
+            score += 1;
+        else score -= 1;
+
+        Destroy(other.gameObject);
+    }
+
+ 
+}
