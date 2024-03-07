cce919f465ef0ee290a732f52574facf0e95182a
marie-anne-decarie <157767089+marie-anne-decarie@users.noreply.github.com>
2024-03-07 16:29:25 -0500

    Worked on interactivity
    Making the words clickable (work in progress)

---
+++ WordsFight_Prototype/Assets/Scripts/Word.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Word : MonoBehaviour
+{
+  
+    public Transform[] slots;
+    public bool[] slotAvailable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        for(int i=0; i<slotAvailable.Length; i++)
+        {
+            if(slotAvailable[i]==true)
+            {
+                this.gameObject.transform.position = slots[i].position;
+                slotAvailable[i] = false;
+                return;
+            }
+        }
+    }
+}
