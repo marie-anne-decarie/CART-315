318058fa86e7e5414661c6e069136cafb5d3c11e
marie-anne-decarie <157767089+marie-anne-decarie@users.noreply.github.com>
2024-03-07 21:00:34 -0500

    Debbuging Practicum
    In class practicum

---
+++ fillaHole_debug-master/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class TimerScript : MonoBehaviour {
+    private Text timerText;
+    private float levelTime;
+    
+    // Start is called before the first frame update
+    void Start() {
+        timerText = GetComponent<Text>();
+        ResetTimer();
+    }
+    
+    public void ResetTimer(){
+        levelTime = 30.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if(levelTime > 0){
+            levelTime -= Time.deltaTime;
+            timerText.text = System.Math.Round(levelTime, 1) + " SECONDS LEFT";       
+        }
+        else {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+}
+
